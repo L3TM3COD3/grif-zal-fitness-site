b3c17b007ba6b2df41dca22b3c8e6790c49c2a28
Noskov Nikita <nosecove@gmail.com>
2018-02-04 17:10:39 +0500

    map started

--- js/lightgallery-settings.js
+++ js/lightgallery-settings.js
@@ -1,3 +1,23 @@
-$(document).ready(function() {
-    $("#lightgallery").lightGallery(); 
+$('#athlete').lightGallery({
+    selector: '.photo-container'
+});
+
+$('#aerobic').lightGallery({
+    selector: '.photo-container'
+});
+
+$('#sauna').lightGallery({
+    selector: '.photo-container'
+});
+
+$('#banya').lightGallery({
+    selector: '.photo-container'
+});
+
+$('#haircut').lightGallery({
+    selector: '.photo-container'
+});
+
+$('#other').lightGallery({
+    selector: '.photo-container'
 });
